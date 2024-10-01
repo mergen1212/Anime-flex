e69459353c85887a7cf44a9381157448b8176a9c
Tiuleev Mergen <tiuleev@sfedu.ru>
2024-10-01 19:41:38 +0300

    add castom title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,10 @@
     export let data;
 
 </script>
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Learn more about our Svelte application" />
+</svelte:head>
 {#each data.posts as i}
     <p>{i.title}</p>
 {/each}
