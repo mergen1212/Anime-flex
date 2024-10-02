093c5ed9cf784d49e7509f3b530f955db614f37f
Tiuleev Mergen <tiuleev@sfedu.ru>
2024-10-02 18:15:13 +0300

    add page uid

--- src/routes/uid/[id]/+page.svelte
+++ src/routes/uid/[id]/+page.svelte
@@ -16,5 +16,60 @@
 		{:else}
 			<p>Loading...</p>
 		{/if}
+		<div>
+			{#if data && data.InfoAnime && data.InfoAnime.data}
+				<p>{data.InfoAnime.data.synopsis}</p>
+			{:else}
+				<p>Loading...</p>
+			{/if}
+		</div>
+	</div>
+	<div class="anime-content">
+		{#if data && data.InfoAnime && data.InfoAnime.data}
+			{#each data.InfoAnime.data.genres as i}
+				<span>{i.name}</span>
+			{/each}
+		{:else}
+			<p>Loading...</p>
+		{/if}
 	</div>
 </div>
+<style>
+	.anime-page {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		min-height: 100vh;
+		background-color: #f5f5f5;
+		padding: 20px;
+	}
+	.anime-header {
+		text-align: center;
+		margin-bottom: 20px;
+	}
+	.anime-header h1 {
+		font-size: 24px;
+		margin-bottom: 10px;
+	}
+	.anime-header img {
+		max-width: 100%;
+		height: auto;
+		margin-bottom: 10px;
+	}
+	.anime-header p {
+		font-size: 16px;
+		margin-bottom: 10px;
+	}
+	.anime-content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		margin-top: 20px;
+	}
+	.anime-content span {
+		font-size: 16px;
+		margin-bottom: 10px;
+	}
+</style>
